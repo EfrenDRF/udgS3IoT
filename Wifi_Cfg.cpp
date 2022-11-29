3275053440c7cec38f2aa0cb6679ff5a5f5049e0
EfrenDRF <efren.delrealf@gmail.com>
2022-11-28 21:36:33 -0600

    first commit - ESP32 connected to Blynk

---
+++ Wifi_Cfg.cpp
@@ -0,0 +1,8 @@
+
+#include "Wifi_Cfg.h"
+
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+extern const char WifiCfg_ssid[ WIFICFG_SSDI_LEN ] = "MY_NETWORK_NAME";
+extern const char WifiCfg_pass[ WIFICFG_PASS_LEN ] = "MY_PASSWORLD";
---
+++ Wifi_Cfg.h
@@ -0,0 +1,19 @@
+
+#ifndef WIFI_CFG_H_
+# define WIFI_CFG_H_
+
+
+#include <stdint.h>
+
+
+# define WIFICFG_SSDI_LEN ( 16u )
+# define WIFICFG_PASS_LEN ( 13u )
+
+
+
+
+extern const char WifiCfg_ssid[ WIFICFG_SSDI_LEN ];
+
+extern const char WifiCfg_pass[ WIFICFG_PASS_LEN ];
+
+#endif
